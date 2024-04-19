708438f5f2682bc383d5b2801c95fb6107765a1c
Kiran-Pro <krc5win@gmail.com>
2024-04-19 19:43:12 +0400

    removed api

--- JS_Thrive/src/pages/LessonPage/LessonContentPage.tsx
+++ JS_Thrive/src/pages/LessonPage/LessonContentPage.tsx
@@ -1,27 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import LessonPage from './LessonPage'; 
-
-interface Lesson {
-  id: number;
-  title: string;
-  linkTo: string;
-}
+import React from 'react';
+import LessonPage from './LessonPage';
+import './LessonPage.css'
 
 const LessonContentPage: React.FC = () => {
-  const [lessons, setLessons] = useState<Lesson[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:4000/lessons')
-      .then(response => response.json())
-      .then(data => setLessons(data))
-      .catch(error => console.error("Fetching lessons failed", error));
-  }, []);
+  
 
   return (
     <div className="lesson-content-container">
-      {lessons.map(lesson => (
-        <LessonPage key={lesson.id} title={lesson.title} linkTo={lesson.linkTo} />
-      ))}
+      <LessonPage title="Lesson 1: Introduction to Web Development and Setup" linkTo="/lesson1" />
+      <LessonPage title="Lesson 2: Adding Structure to the Quiz App" linkTo="/lesson2" />
+      <LessonPage title="Lesson 3: Making the Quiz Interactive with JavaScript" linkTo="/lesson3" />
+      <LessonPage title="Lesson 4: Enhancing the Quiz with Styling" linkTo="/lesson4" />
+      <LessonPage title="Lesson 5: Adding Feedback and Score Tracking" linkTo="/lesson5" />
+      <LessonPage title="Lesson 6: Implementing Quiz Restart and High Score Tracking" linkTo="/lesson6" />
+      <LessonPage title="Lesson 7: Enhancing the Quiz with Local Storage for Persistent Data" linkTo="/lesson7" />
+      <LessonPage title="Lesson 8: Implementing Answer Validation and Feedback" linkTo="/lesson8" />
+      <LessonPage title="Lesson 9: Enhancing User Experience and Finalizing the Quiz App" linkTo="/lesson9" />
     </div>
   );
 };
