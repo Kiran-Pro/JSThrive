f4088ecd42e4853cc4d2899a17cb928fe6e193ae
Anonymous <krc5win@gmail.com>
2024-05-22 00:02:53 +0400

    certificate_fix2

--- JS_Thrive/src/AppRoutes.tsx
+++ JS_Thrive/src/AppRoutes.tsx
@@ -1,37 +1,42 @@
-import { Suspense, lazy } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { NotificationProvider } from './Context/NotificationContext';
-import { ThemeProvider } from './Context/ThemeContext';
-import Loader from './components/Loader/Loader';
-import Navbar from './components/Navbar/Navbar';
+import { Suspense, lazy } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { NotificationProvider } from "./Context/NotificationContext";
+import { ThemeProvider } from "./Context/ThemeContext";
+import Loader from "./components/Loader/Loader";
+import Navbar from "./components/Navbar/Navbar";
 
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage/ProfilePage"));
+const Register = lazy(() => import("./pages/UserAuthentication/Register"));
+const Login = lazy(() => import("./pages/UserAuthentication/Login"));
+const ForgetPassword = lazy(
+  () => import("./pages/UserAuthentication/ForgetPassword")
+);
+const NotFoundPage = lazy(() => import("./pages/ErrorPage/NotFoundPage"));
+const LessonContainer = lazy(
+  () => import("./pages/LessonPage/LessonContainer")
+);
+const CertificatePage = lazy(
+  () => import("./pages/ProfilePage/CertificatePage")
+);
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const ProfilePage = lazy(() => import('./pages/ProfilePage/ProfilePage'));
-const Register = lazy(() => import('./pages/UserAuthentication/Register'));
-const Login = lazy(() => import('./pages/UserAuthentication/Login'));
-const ForgetPassword = lazy(() => import('./pages/UserAuthentication/ForgetPassword'));
-const NotFoundPage = lazy(() => import('./pages/ErrorPage/NotFoundPage'));
-const LessonContainer = lazy(() => import('./pages/LessonPage/LessonContainer'));
-const CertificatePage = lazy(() => import('./pages/ProfilePage/CertificatePage'));
+const Lesson1 = lazy(() => import("./pages/LessonPage/Lessons/lesson1"));
+const Lesson2 = lazy(() => import("./pages/LessonPage/Lessons/lesson2"));
+const Lesson3 = lazy(() => import("./pages/LessonPage/Lessons/lesson3"));
+const Lesson4 = lazy(() => import("./pages/LessonPage/Lessons/lesson4"));
+const Lesson5 = lazy(() => import("./pages/LessonPage/Lessons/lesson5"));
+const Lesson6 = lazy(() => import("./pages/LessonPage/Lessons/lesson6"));
+const Lesson7 = lazy(() => import("./pages/LessonPage/Lessons/lesson7"));
+const Lesson8 = lazy(() => import("./pages/LessonPage/Lessons/lesson8"));
+const Lesson9 = lazy(() => import("./pages/LessonPage/Lessons/lesson9"));
 
-const Lesson1 = lazy(() => import('./pages/LessonPage/Lessons/Lesson1'));
-const Lesson2 = lazy(() => import('./pages/LessonPage/Lessons/Lesson2'));
-const Lesson3 = lazy(() => import('./pages/LessonPage/Lessons/Lesson3'));
-const Lesson4 = lazy(() => import('./pages/LessonPage/Lessons/Lesson4'));
-const Lesson5 = lazy(() => import('./pages/LessonPage/Lessons/Lesson5'));
-const Lesson6 = lazy(() => import('./pages/LessonPage/Lessons/Lesson6'));
-const Lesson7 = lazy(() => import('./pages/LessonPage/Lessons/Lesson7'));
-const Lesson8 = lazy(() => import('./pages/LessonPage/Lessons/Lesson8'));
-const Lesson9 = lazy(() => import('./pages/LessonPage/Lessons/Lesson9'));
-
-
-const GamePage = lazy(() => import('./pages/GamePage/GamePage'));
-const Game2 = lazy(() => import('./pages/GamePage/Games/TicTacToe/TicTacToe'));
-const Game3 = lazy(() => import('./pages/GamePage/Games/RockPaper/RockPaper'));
-const Game4 = lazy(() => import('./pages/GamePage/Games/NumberGuessing/NumberGuessing'));
-const Game5 = lazy(() => import('./pages/GamePage/Games/Snake/Snake'));
-
+const GamePage = lazy(() => import("./pages/GamePage/GamePage"));
+const Game2 = lazy(() => import("./pages/GamePage/Games/TicTacToe/TicTacToe"));
+const Game3 = lazy(() => import("./pages/GamePage/Games/RockPaper/RockPaper"));
+const Game4 = lazy(
+  () => import("./pages/GamePage/Games/NumberGuessing/NumberGuessing")
+);
+const Game5 = lazy(() => import("./pages/GamePage/Games/Snake/Snake"));
 
 const AppRoutes = () => {
   return (
@@ -53,12 +58,12 @@ const AppRoutes = () => {
               <Route path="/lesson8" element={<Lesson8 />} />
               <Route path="/lesson9" element={<Lesson9 />} />
               <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/certificate" element={<CertificatePage/>}/>
-              <Route path="/gamepage" element={<GamePage/>}/>
-              <Route path="/game2" element={<Game2/>}/>
-              <Route path="/game3" element={<Game3/>}/>
-              <Route path="/game4" element={<Game4/>}/>
-              <Route path="/game5" element={<Game5/>}/>
+              <Route path="/certificate" element={<CertificatePage />} />
+              <Route path="/gamepage" element={<GamePage />} />
+              <Route path="/game2" element={<Game2 />} />
+              <Route path="/game3" element={<Game3 />} />
+              <Route path="/game4" element={<Game4 />} />
+              <Route path="/game5" element={<Game5 />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
               <Route path="/forgetPass" element={<ForgetPassword />} />
--- JS_Thrive/src/pages/ProfilePage/Certificate.tsx
+++ JS_Thrive/src/pages/ProfilePage/Certificate.tsx
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-import './Certificate.css';
-import stamp from '../../assets/stamp.png'
-import sign from '../../assets/signature.png'
+import React, { useRef } from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import "./Certificate.css";
+import stamp from "../../assets/stamp.png";
+import sign from "../../assets/signature.png";
 
 interface User {
   displayName: string | null;
@@ -20,32 +20,56 @@ const Certificate: React.FC<CertificateProps> = ({ user }) => {
   const generatePDF = () => {
     const input = certificateRef.current;
     if (input) {
-      html2canvas(input).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
+      html2canvas(input, { scale: 2 }).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
         const pdf = new jsPDF({
-          orientation: 'landscape',
+          orientation: "landscape",
+          unit: "mm",
+          format: [canvas.width * 0.264583, canvas.height * 0.264583],
         });
-        pdf.addImage(imgData, 'PNG', 0, 0, 297, 210);
-        pdf.save('certificate.pdf');
+
+        const width = pdf.internal.pageSize.getWidth();
+        const height = pdf.internal.pageSize.getHeight();
+        pdf.addImage(imgData, "PNG", 0, 0, width, height);
+        pdf.save("certificate.pdf");
       });
     }
   };
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
       <div className="certificate" ref={certificateRef}>
         <div className="certificate-content">
           <h1>Certificate of Completion</h1>
           <p>This certificate is proudly presented to</p>
-          <h2>{user.displayName || 'Unknown'}</h2>
+          <h2>{user.displayName || "Unknown"}</h2>
           <p>For successfully completing the</p>
           <h3>JSThrive: JavaScript Basics Learning</h3>
           <p>Date: {new Date().toLocaleDateString()}</p>
-          <img width={150} style={{position:'relative',left:'480px'}} src={sign} alt="" />
-          <img width={200} style={{marginTop:'-6rem',marginLeft:'19rem'}} src={stamp} alt="" />
+          <img
+            width={150}
+            style={{ position: "relative", left: "480px" }}
+            src={sign}
+            alt=""
+          />
+          <img
+            width={200}
+            style={{ marginTop: "-6rem", marginLeft: "19rem" }}
+            src={stamp}
+            alt=""
+          />
         </div>
       </div>
-      <button onClick={generatePDF}>Download Certificate</button>
+      <button className="button" onClick={generatePDF}>
+        Download Certificate
+      </button>
     </div>
   );
 };
